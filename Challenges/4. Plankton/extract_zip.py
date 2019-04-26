94c8d7e9c2e9a1942a7c79983a8a89f9f975d756
eivinhoy <eivind@hoydal.net>
2019-04-26 12:18:51 +0000

    back to two notebooks

--- Challenges/4. Plankton/extract_zip.py
+++ Challenges/4. Plankton/extract_zip.py
@@ -15,14 +15,19 @@ def extract_zip_to_memory(input_zip):
     return {name: BytesIO(input_zip.read(name)) for name in input_zip.namelist() if name.endswith('.jpg')}
 
 
-# img_files = extract_zip_to_memory("imgs.zip")
+# imgFiles = extract_zip_to_memory("imgs.zip")
 
 # Display an example image 
-# Image.open(img_files['imgs/32738710.jpg'])
+# Image.open(imgFiles['imgs/32738710.jpg'])
 
 # Load the image as a numpy array:
-# np_arr = np.array(Image.open(img_files['imgs/32738710.jpg']))
+# np_arr = np.array(Image.open(imgFiles['imgs/32738710.jpg']))
 
 # Be aware that the dictionary will occupy roughly 2GB of computer memory!
 # To free this memory again, run:
-# del img_file
+# del imgFiles
+
+'''ZipFile.read(name[, pwd])
+Return the bytes of the file name in the archive. name is the name of the file in the archive, or a ZipInfo object. The archive must be open for read or append. pwd is the password used for encrypted files and, if specified, it will override the default password set with setpassword(). Calling read() on a closed ZipFile will raise a RuntimeError.'''
+
+
